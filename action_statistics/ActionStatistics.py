df9929b4353249f2b715077b23fcfcc33c6727de
Ryan Steele <imacube@gmail.com>
2020-12-03 15:51:14 -0800

    Reworked to use threading

--- action_statistics/ActionStatistics.py
+++ action_statistics/ActionStatistics.py
@@ -1,11 +1,31 @@
 """ActionStatistics stores the average time for each of the provided actions."""
-from multiprocessing import Manager
+import threading
+from json import loads, dumps  # minor performance improvement not calling a . sub-method
+from queue import SimpleQueue
+from statistics import mean
+from time import sleep
 
 
 class ActionStatistics:
     def __init__(self):
-        manager = Manager()
-        self._dict = manager.dict()
+        self.queue_add_action = SimpleQueue()
+        self.queue_get_stats = SimpleQueue()
+        self.queue_get_stats_response = SimpleQueue()
+        self._dict = dict()
+        threading.Thread(target=self.worker, daemon=True).start()
+
+    def worker(self):
+        """Manages queue needs."""
+        while True:
+            if self.queue_add_action.empty() and self.queue_get_stats.empty():
+                sleep(0.5)
+                continue
+            if not self.queue_add_action.empty():
+                item = self.queue_add_action.get_nowait()
+                if item:
+                    self._add_action(item)
+            if not self.queue_get_stats.empty() and self.queue_get_stats.get_nowait():
+                self.queue_get_stats_response.put(self._get_stats())
 
     def addAction(self, action: str):
         """Add an action's time to the average for that action.
@@ -15,7 +35,7 @@ class ActionStatistics:
         action : str
             A JSON serialized string.
         """
-        pass
+        self.queue_add_action.put(action)
 
     def getStats(self):
         """Return the average times for each action.
@@ -25,4 +45,29 @@ class ActionStatistics:
         str
             JSON serialized string containing a list of the actions and their respective average times.
         """
-        pass
+        self.queue_get_stats.put(True)
+        return self.queue_get_stats_response.get(block=True, timeout=5)
+
+    def _add_action(self, action: str):
+        """Add an action to the internal storage object.
+
+        Parameter
+        ---------
+        action : str
+            JSON serialized action to add.
+        """
+        action = loads(action)
+        action_time = action['time']
+        action = action['action']
+        internal_action = self._dict.get(action, list())
+        internal_action.append(int(action_time))
+        self._dict[action] = internal_action
+
+    def _get_stats(self):
+        """Return the stats."""
+        return dumps(
+            [
+                {'action': k, 'avg': mean(v)} for k, v in
+                self._dict.items()
+            ]
+        )
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 """Basic tests for this module."""
+import threading
 import unittest
-from multiprocessing import Process
+from json import loads, dumps
 from random import randint
+from statistics import mean
 from time import sleep
 
 from action_statistics.ActionStatistics import ActionStatistics
@@ -19,7 +21,7 @@ class TestConcurrent(unittest.TestCase):
     """Test concurrency."""
 
     @staticmethod
-    def sub_process(action_stats, add_action: dict, sleep_window=0):
+    def sub_process(action_stats: ActionStatistics, add_action: dict, sleep_window=0):
         """Action to be taken by a subprocess.
 
         Parameter
@@ -34,50 +36,92 @@ class TestConcurrent(unittest.TestCase):
         if sleep_window > 0:
             sleep(randint(1, sleep_window))
 
-        for key, value in add_action.items():
-            action_stats._dict[key] = value
-            history = action_stats._dict.get(f'history_{key}', list())
-            history.append(value)
-            action_stats._dict[f'history_{key}'] = history
+        for action in add_action:
+            action_stats.addAction(dumps(action))
 
-    def test_access_dict(self):
+    def test_empty(self):
         """Test accessing the shared dictionary."""
         action_stats = ActionStatistics()
-        action_stats._dict['a'] = 1
+        assert action_stats.getStats() == '[]'
 
-        assert action_stats._dict['a'] == 1
-
-    def test_sub_process(self):
-        """Test a subprocess accessing the shared dict."""
-
+    def test_add_action(self):
+        """Test the internal _add_action method."""
         action_stats = ActionStatistics()
-        p = Process(target=self.sub_process, args=(action_stats, {'abc': 123}, 3))
-        p.start()
-        p.join()
+        action_stats._add_action('''{"action":"jump", "time":100}''')
+        action_stats._add_action('''{"action":"jump", "time":200}''')
+        assert action_stats._dict == {'jump': [100, 200]}
 
-        assert action_stats._dict['abc'] == 123
-
-    def test_multiple_sub_processes(self):
-        """Test multiple subprocesses accessing the shared dict."""
-
+    def test_get_stats(self):
+        """Test the internal _get_stats method."""
         action_stats = ActionStatistics()
-        processes = list()
-        for d in [{'abc': x} for x in range(100)]:
-            p = Process(target=self.sub_process, args=(action_stats, d, 0))
-            processes.append(p)
+        action_stats._dict = {'jump': [100]}
+        assert loads(action_stats._get_stats()) == [{"action": "jump", "avg": 100}]
 
-        for p in processes:
-            p.start()
-        for p in processes:
-            p.join()
+    def test_addAction(self):
+        """Test adding an item."""
+        action_stats = ActionStatistics()
+        action_stats.addAction('''{"action":"jump", "time":100}''')
+        sleep(2)
+        assert action_stats._dict == {'jump': [100]}
 
-        history = action_stats._dict['history_abc']
-        # todo provide a means to automatically rerun the test if this should happen
-        assert history != [x for x in range(100)]  # If this does happen by accident, then just re-run the tests
-        for x in range(100):
-            if x not in history:
-                print(f'\nMissing value: {x}')
-        assert set(history) == {x for x in range(100)}
+    def test_getStats(self):
+        """Test getting an item."""
+        action_stats = ActionStatistics()
+        action_stats._dict = {'jump': [100]}
+        assert loads(action_stats.getStats()) == [{"action": "jump", "avg": 100}]
+
+    def test_addAction_multiple(self):
+        """Test that the average is being calculated correctly."""
+        action_stats = ActionStatistics()
+        action_stats.addAction('''{"action":"jump", "time":100}''')
+        action_stats.addAction('''{"action":"jump", "time":200}''')
+        sleep(2)
+        assert action_stats.getStats() == '''[{"action": "jump", "avg": 150}]'''
+
+    def test_thread(self):
+        """Test a thread accessing the shared dict."""
+        action_stats = ActionStatistics()
+        thread = threading.Thread(target=self.sub_process, args=(
+            action_stats,
+            [
+                {"action": "jump", "time": 100},
+                {"action": "run", "time": 75},
+                {"action": "jump", "time": 200}
+            ],
+            3
+        ))
+        thread.start()
+        thread.join()
+        sleep(2)
+        assert action_stats._dict == {'jump': [100, 200], 'run': [75]}
+        assert action_stats.getStats() == dumps([{"action":"jump", "avg":150}, {"action":"run", "avg":75}])
+
+    def test_multiple_threads(self):
+        """Test multiple threads accessing the method."""
+
+        def trial_run():
+            _action_stats = ActionStatistics()
+            threads = list()
+            for d in [[{'action': 'jump', 'time': x}] for x in range(100)]:
+                t = threading.Thread(target=self.sub_process, args=(_action_stats, d, 5))
+                threads.append(t)
+
+            for t in threads:
+                t.start()
+            for t in threads:
+                t.join()
+
+            sleep(10)
+            assert mean(_action_stats._dict['jump']) == 49.5
+            return _action_stats
+
+        # If the values are not in a random order, re-run the test
+        for _ in range(5):
+            action_stats = trial_run()
+            if action_stats._dict['jump'] != [x for x in
+                                              range(100)]:
+                break
+        assert action_stats._dict['jump'] != [x for x in range(100)]
 
 
 if __name__ == '__main__':
